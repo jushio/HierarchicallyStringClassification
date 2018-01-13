3792b0c896b19a6553fbdc00a23408ee275ffec6
ushio <ushio@nii.ac.jp>
2018-01-13 18:12:32 +0900

    first

---
+++ HierarchicallyStringClassfication/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HierarchicallyStringClassfication
+{
+    internal class Node
+    {
+        string Str;
+        int NofDescendants = 0;
+        Dictionary<string, Node> Children = new Dictionary<string, Node>();
+
+        internal Node(string word)
+        {
+            Str = word;
+        }
+
+        internal void AddNode(IEnumerable<string> strs)
+        {
+            NofDescendants++;
+            if (strs.Count() == 0) return;
+            if (!Children.ContainsKey(strs.First()))
+                Children[strs.First()] = new Node(strs.First());
+            Children[strs.First()].AddNode(strs.Skip(1));
+        }
+
+        //リストアップする
+        internal void ListUp(int depth = 0, int limit = int.MaxValue)
+        {
+            if (depth > limit)
+                return;
+            for (int i = 0; i < depth; i++) Console.Write("-");
+            Print();
+            Children.Values.ToList().ForEach(n => n.ListUp(depth+1, limit));
+        }
+
+        internal void Print()
+        {
+            Console.WriteLine(Str + " : " + NofDescendants);
+        }
+    }
+}
---
+++ HierarchicallyStringClassfication/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HierarchicallyStringClassfication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (false)
+                Test();
+
+            string dataDir = @"./dat";
+            string[] files = System.IO.Directory.GetFiles(
+                dataDir, "*", System.IO.SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+                StreamWriter sw = new StreamWriter(
+                file + "out.txt", // 出力先ファイル名
+                  true, // 追加書き込み
+                System.Text.Encoding.GetEncoding("Shift_JIS")); // 文字コード
+                TextWriter tmp = Console.Out; // 標準出力の保持
+                Console.SetOut(sw); // 出力先（Outプロパティ）を設定
+
+                ClassifyFile(file, 4);
+                Console.SetOut(tmp); //戻す
+                sw.Close();
+            }
+        }
+
+        static void ClassifyFile(string fileName, int limit = int.MaxValue)
+        {
+            var root = new Node("");
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(fileName);
+            string line;
+            var delims = new char[] { '/', '_' };
+            while ((line = file.ReadLine()) != null)
+            {
+                root.AddNode(line.Split(delims));
+            }
+            root.ListUp(0, limit);
+        }
+
+        static void Test()
+        {
+            List<string> sample = new List<string>() { "a", "b", "a" };
+            List<List<string>> samples = new List<List<string>>()
+            {
+                new List<string>() {"a", "b" },
+                new List<string>() {"a", "c" },
+                new List<string>() {"b", "c" }
+            };
+            var root = new Node("");
+            root.AddNode(sample);
+            root.ListUp(0);
+            Console.ReadLine();
+
+            var root2 = new Node("");
+            foreach (var sa in samples)
+                root2.AddNode(sa);
+            root2.ListUp(0);
+            Console.ReadLine();
+
+            List<string> sample3 = new List<string>()
+            {
+                "a/b/c",
+                "a/b/a",
+                "a/b/d"
+            };
+            var root3 = new Node("");
+            foreach (var sa in sample3)
+                root3.AddNode(sa.Split('/'));
+            root3.ListUp();
+            Console.ReadLine();
+
+            ClassifyFile(@"0.txt", 5);
+        }
+    }
+}
